691f63be14aa7f8dbaa958c8993e348d03618a8b
Alexey Zlobin <alexey.zlobin@gmail.com>
2019-09-04 14:52:14 +0300

    Parser for scene WIP

--- src/main/kotlin/Reader.kt
+++ src/main/kotlin/Reader.kt
@@ -1,89 +1,118 @@
 package cc.cheatex.ktracer
 
+import com.github.h0tk3y.betterParse.combinators.and
+import com.github.h0tk3y.betterParse.combinators.map
+import com.github.h0tk3y.betterParse.combinators.skip
+import com.github.h0tk3y.betterParse.grammar.Grammar
+import com.github.h0tk3y.betterParse.lexer.TokenMatch
+import com.github.h0tk3y.betterParse.parser.ErrorResult
+import com.github.h0tk3y.betterParse.parser.ParseResult
+import com.github.h0tk3y.betterParse.parser.Parser
 import java.io.BufferedReader
 import java.io.InputStream
 import java.io.InputStreamReader
 import java.util.*
 
+object NotImplementedResult: ErrorResult()
+
+object NotImplemented: Parser<Nothing> {
+  override fun tryParse(tokens: Sequence<TokenMatch>): ParseResult<Nothing> = NotImplementedResult
+}
+
+object ItemsParser : Grammar<Scene>() {
+  val num       by token("-?\\d+")
+  val floatNum  by token("(-?\\d+(\\.\\d*)?|\\d*\\.\\d+)", ignore = true)
+  val open      by token("\\{")
+  val close     by token("}")
+  val eq        by token("=")
+  val scene     by token("scene")
+
+  val SceneBody: Parser<Scene> = NotImplemented
+  val Scene: Parser<Scene> = skip(scene) and skip(open) and SceneBody and skip(close) map { scene -> scene }
+
+  override val rootParser: Parser<Scene>
+    get() = Scene
+}
+
 fun parseScene(input: InputStream): Scene {
-    val reader = BufferedReader(InputStreamReader(input))
+  val reader = BufferedReader(InputStreamReader(input))
 
-    val background = readColor(reader)
-    val ambientLight = readColor(reader)
-    val camera = readCamera(reader)
-    val lights = readLights(reader)
-    val objects = readObjects(reader)
+  val background = readColor(reader)
+  val ambientLight = readColor(reader)
+  val camera = readCamera(reader)
+  val lights = readLights(reader)
+  val objects = readObjects(reader)
 
-   return Scene(camera, objects, lights, background, ambientLight)
+  return Scene(camera, objects, lights, background, ambientLight)
 }
 
 private fun readCamera(reader: BufferedReader): Camera {
-    val name = reader.readLine().trim()
-    val at = readVector(reader)
-    val up = readVector(reader)
-    val viewport = reader.readLine().trim().toDouble()
+  val name = reader.readLine().trim()
+  val at = readVector(reader)
+  val up = readVector(reader)
+  val viewport = reader.readLine().trim().toDouble()
 
-    return Camera(at, up, viewport)
+  return Camera(at, up, viewport)
 }
 
 private fun readLights(reader: BufferedReader): List<Light> =
     readObjectGroup(reader, ::readPointLight) +
-    readObjectGroup(reader, ::readSpotLight)
+        readObjectGroup(reader, ::readSpotLight)
 
 private fun readObjects(reader: BufferedReader): List<MaterialObject> =
     readObjectGroup(reader, ::readSphere)
 
 private fun readPointLight(reader: BufferedReader): PointLight {
-    val name = reader.readLine().trim()
-    val position = readVector(reader)
-    val intensity = readColor(reader)
+  val name = reader.readLine().trim()
+  val position = readVector(reader)
+  val intensity = readColor(reader)
 
-   return PointLight(position, intensity)
+  return PointLight(position, intensity)
 }
 
 private fun readSpotLight(reader: BufferedReader): SpotLight {
-    val name = reader.readLine().trim()
-    val position = readVector(reader)
-    val at = readVector(reader)
-    val angle = reader.readLine().trim().toDouble()
-    val intensity = readColor(reader)
+  val name = reader.readLine().trim()
+  val position = readVector(reader)
+  val at = readVector(reader)
+  val angle = reader.readLine().trim().toDouble()
+  val intensity = readColor(reader)
 
-   return SpotLight(position, at, angle, intensity)
+  return SpotLight(position, at, angle, intensity)
 }
 
 private fun <T, R : T> readObjectGroup(reader: BufferedReader,
-                               objectReader: (BufferedReader) -> R): List<T> {
-    reader.readLine()
-    val objectsCount = reader.readLine().trim().toInt()
+                                       objectReader: (BufferedReader) -> R): List<T> {
+  reader.readLine()
+  val objectsCount = reader.readLine().trim().toInt()
 
-    return sequence {
-        for (i in 0..objectsCount) yield(objectReader(reader))
-    } .toList()
+  return sequence {
+    for (i in 0..objectsCount) yield(objectReader(reader))
+  } .toList()
 }
 
 private fun readSphere(reader: BufferedReader): Sphere {
-    val name = reader.readLine().trim()
-    val position = readVector(reader)
-    val radius = reader.readLine().trim().toDouble()
-    val material = readMaterial(reader)
+  val name = reader.readLine().trim()
+  val position = readVector(reader)
+  val radius = reader.readLine().trim().toDouble()
+  val material = readMaterial(reader)
 
-   return Sphere(position, material, radius)
+  return Sphere(position, material, radius)
 }
 
 private fun readMaterial(reader: BufferedReader): Material {
-    return Material(ColorD.white, ColorD.white, ColorD.white, 1.0, 1.0, 1.0, 1.0, 1.0)
+  return Material(ColorD.white, ColorD.white, ColorD.white, 1.0, 1.0, 1.0, 1.0, 1.0)
 }
 
 private fun readVector(reader: BufferedReader): VectorD {
-    val tokenizer = StringTokenizer(reader.readLine())
-    return VectorD(tokenizer.nextToken().toDouble(),
-            tokenizer.nextToken().toDouble(),
-            tokenizer.nextToken().toDouble())
+  val tokenizer = StringTokenizer(reader.readLine())
+  return VectorD(tokenizer.nextToken().toDouble(),
+      tokenizer.nextToken().toDouble(),
+      tokenizer.nextToken().toDouble())
 }
 
 private fun readColor(reader: BufferedReader): ColorD {
-    val tokenizer = StringTokenizer(reader.readLine())
-    return ColorD(tokenizer.nextToken().toDouble(),
-            tokenizer.nextToken().toDouble(),
-            tokenizer.nextToken().toDouble())
+  val tokenizer = StringTokenizer(reader.readLine())
+  return ColorD(tokenizer.nextToken().toDouble(),
+      tokenizer.nextToken().toDouble(),
+      tokenizer.nextToken().toDouble())
 }
